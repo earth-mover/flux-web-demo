d04afb91342e308fef04b6576f69bc065049abe7
Matthew Iannucci <matthew@earthmover.io>
2025-05-22 13:30:43 -0400

    Slow loading, but working particles on globe

--- src/demos/globe.tsx
+++ src/demos/globe.tsx
@@ -2,21 +2,119 @@ import Map, { Layer, Source, useControl } from 'react-map-gl/maplibre';
 import { MapboxOverlay as DeckOverlay, MapboxOverlayProps } from '@deck.gl/mapbox';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import { Layer as DeckLayer } from '@deck.gl/core';
+import { useQuery } from '@tanstack/react-query';
+import * as WeatherLayers from 'weatherlayers-gl';
 
+/**
+ * Creates a WMS URL template for fetching GFS wind data from Flux
+ * @param colorPalette - The color palette to use. This is any matplotlib colormap name. See https://matplotlib.org/stable/tutorials/colors/colormaps.html
+ * @param colorScaleRange - The range of the color scale, formatted as [min, max].
+ * @param layer - The data variable to use.
+ * @param tileWidth - The width of the tile.
+ * @param tileHeight - The height of the tile.
+ * @param x - The x coordinate of the tile. If creating a tile template leave undefined.
+ * @param y - The y coordinate of the tile. If creating a tile template leave undefined.
+ * @param z - The z coordinate of the tile. If creating a tile template leave undefined.
+ */
 function createGfsWmsUrlTemplate({
     colorPalette,
     colorScaleRange,
     layer,
-    tileSize,
+    tileWidth,
+    tileHeight,
+    x,
+    y,
+    z,
 }: {
     colorPalette: string;
     colorScaleRange: [number, number];
     layer: 'temperature_2m' | 'wind_u_10m' | 'wind_v_10m';
-    tileSize: number;
+    tileWidth: number;
+    tileHeight: number;
+    x?: number;
+    y?: number;
+    z?: number;
 }): string {
+    const xx = x ?? '{x}';
+    const yy = y ?? '{y}';
+    const zz = z ?? '{z}';
     return `https://compute.earthmover.io/v1/services/wms/earthmover-demos/dyanmical-gfs-analysis/main/wms?version=1.3.0&service=WMS&request=GetMap&layers=${layer}&colorscalerange=${colorScaleRange.join(
         ',',
-    )}&width=${tileSize}&height=${tileSize}&tile={x},{y},{z}&crs=EPSG:3857&styles=raster/${colorPalette}`;
+    )}&width=${tileWidth}&height=${tileHeight}&tile=${xx},${yy},${zz}&crs=EPSG:3857&styles=raster/${colorPalette}`;
+}
+
+/**
+ * Fetches the global GFS wind data for the current time step and creates a bipped image to be used for drawing animated particles.
+ * See https://docs.weatherlayers.com/weatherlayers-gl/data-sources for more information on the data source specification..
+ *
+ * @returns The wind data for the current time step, combining the u and v wind components into a single image.
+ */
+async function fetchGlobalGfsWindParticleData({
+    dataRange,
+    imageWidth,
+    imageHeight,
+}: {
+    dataRange: [number, number];
+    imageWidth: number;
+    imageHeight: number;
+}) {
+    const uwindUrl = createGfsWmsUrlTemplate({
+        colorPalette: 'Greys_r',
+        colorScaleRange: dataRange,
+        layer: 'wind_u_10m',
+        tileWidth: imageWidth,
+        tileHeight: imageHeight,
+        x: 0,
+        y: 0,
+        z: 0,
+    });
+    const vwindUrl = createGfsWmsUrlTemplate({
+        colorPalette: 'Greys_r',
+        colorScaleRange: dataRange,
+        layer: 'wind_v_10m',
+        tileWidth: imageWidth,
+        tileHeight: imageHeight,
+        x: 0,
+        y: 0,
+        z: 0,
+    });
+    const [uwind, vwind] = await Promise.all([fetch(uwindUrl), fetch(vwindUrl)]);
+    const [uwindData, vwindData] = await Promise.all([uwind.arrayBuffer(), vwind.arrayBuffer()]);
+
+    // Create ImageBitmap objects from the array buffers
+    const [uBitmap, vBitmap] = await Promise.all([
+        createImageBitmap(new Blob([uwindData], { type: 'image/png' })),
+        createImageBitmap(new Blob([vwindData], { type: 'image/png' })),
+    ]);
+
+    // Create canvases to read pixel data
+    const uCanvas = new OffscreenCanvas(uBitmap.width, uBitmap.height);
+    const vCanvas = new OffscreenCanvas(vBitmap.width, vBitmap.height);
+    const uCtx = uCanvas.getContext('2d');
+    const vCtx = vCanvas.getContext('2d');
+
+    if (!uCtx || !vCtx) {
+        throw new Error('Failed to get canvas context');
+    }
+
+    // Draw the bitmaps to canvases
+    uCtx.drawImage(uBitmap, 0, 0);
+    vCtx.drawImage(vBitmap, 0, 0);
+
+    // Get pixel data
+    const uImageData = uCtx.getImageData(0, 0, uCanvas.width, uCanvas.height);
+    const vImageData = vCtx.getImageData(0, 0, vCanvas.width, vCanvas.height);
+
+    // Modify U data array directly, only copying G channel from V
+    for (let i = 0; i < uImageData.data.length; i += 4) {
+        uImageData.data[i + 1] = vImageData.data[i]; // G from V
+    }
+
+    return {
+        data: uImageData.data,
+        width: uBitmap.width,
+        height: uBitmap.height,
+    };
 }
 
 function DeckGLOverlay(props: MapboxOverlayProps) {
@@ -29,27 +127,30 @@ function DeckGLOverlay(props: MapboxOverlayProps) {
 }
 
 export default function Globe() {
-    const layers: DeckLayer[] = [
-        // new TileLayer({
-        //     id: 'gfs-temperature',
-        //     data: 'https://compute.earthmover.io/v1/services/wms/earthmover-demos/dyanmical-gfs-analysis/main/wms?version=1.3.0&service=WMS&request=GetMap&layers=temperature_2m&colorscalerange=0,50&width=512&height=512&tile={x},{y},{z}&crs=EPSG:3857&styles=raster/viridis',
-        //     tileSize: 512,
-        //     maxRequests: 8,
-        //     opacity: 0.5,
-        //     refinementStrategy: 'no-overlap',
-        //     beforeId: '"housenumber"',
-        //     renderSubLayers: (props) => {
-        //         const [[west, south], [east, north]] = props.tile.boundingBox;
-        //         const { data, ...otherProps } = props;
-        //         return [
-        //             new BitmapLayer(otherProps, {
-        //                 image: data,
-        //                 bounds: [west, south, east, north],
-        //             }),
-        //         ];
-        //     },
-        // }),
-    ];
+    const { data } = useQuery({
+        queryKey: ['gfs-wind-bipped'],
+        queryFn: async () =>
+            await fetchGlobalGfsWindParticleData({
+                dataRange: [-40, 40],
+                imageWidth: 1440,
+                imageHeight: 720,
+            }),
+    });
+
+    const layers: DeckLayer[] = data
+        ? [
+              new WeatherLayers.ParticleLayer({
+                  id: 'gfs-wind',
+                  image: data,
+                  imageType: 'VECTOR',
+                  imageUnscale: [-40, 40],
+                  maxAge: 10,
+                  speedFactor: 3.0,
+                  numParticles: 6000,
+              }),
+          ]
+        : [];
+
     return (
         <main className="h-screen w-screen">
             <Map
@@ -81,10 +182,11 @@ export default function Globe() {
                     type="raster"
                     tiles={[
                         createGfsWmsUrlTemplate({
-                            colorPalette: 'gist_earth',
+                            colorPalette: 'jet',
                             colorScaleRange: [-40, 40],
                             layer: 'temperature_2m',
-                            tileSize: 512,
+                            tileWidth: 512,
+                            tileHeight: 512,
                         }),
                     ]}
                 >
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router";
-import Globe from "./demos/globe";
+import './index.css';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Globe from './demos/globe';
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Globe />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Globe />} />
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
+    </StrictMode>,
 );
